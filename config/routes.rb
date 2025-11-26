d02c06d8ae6876f5bde016c8b40f245489102df7
Robert Golusinski <robertgolusinski@gmail.com>
2025-11-26 18:35:27 +0100

    made it mobile-first

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get "up" => "rails/health#show", as: :rails_health_check
+  get "up" => "rails/health#show", :as => :rails_health_check
 
   root "games#new"
 
-  post "score" => "games#create",  as: :create_score
+  post "score" => "games#create", :as => :create_score
   get "score" => "games#score"
 end
